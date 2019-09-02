c7c72f99c214e76d8e8ec9771a362f2e6fd9d98e
webchills <office@webchills.com>
2019-09-02 20:31:14 +0200

    version updates and fixes

--- NEUE DATEIEN/DEINADMIN/includes/extra_datafiles/flexible_footer_sanitizer.php
+++ NEUE DATEIEN/DEINADMIN/includes/extra_datafiles/flexible_footer_sanitizer.php
@@ -4,7 +4,7 @@
  * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: flexible_footer_sanitize.php 2019-04-19 18:13:51Z webchills $
+ * @version $Id: flexible_footer_sanitize.php 2019-09-02 20:13:51Z webchills $
  */
 if (class_exists('AdminRequestSanitizer')) {
   $sanitizer = AdminRequestSanitizer::getInstance();
@@ -21,7 +21,7 @@ if (class_exists('AdminRequestSanitizer')) {
       'method' => 'both',
       'pages' => array('flexible_footer_menu'),
       'params' => array()),
-    'page_url' => array('sanitizerType' => 'PRODUCT_URL_REGEX',
+    'page_url' => array('sanitizerType' => 'PRODUCT_DES_REGEX',
       'method' => 'both',
       'pages' => array('flexible_footer_menu'),
       'params' => array()),
