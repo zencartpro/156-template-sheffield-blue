eb7dd5637f79277a7dd67d69406023460c2c4df9
webchills <office@webchills.com>
2019-11-14 09:11:03 +0100

    Fixes aud 1.5.6d integriert

--- NEUE DATEIEN/includes/templates/responsive_sheffield_blue/template_info.php
+++ NEUE DATEIEN/includes/templates/responsive_sheffield_blue/template_info.php
@@ -1,6 +1,6 @@
 <?php
 $template_name = 'Responsive Sheffield Blue';
-$template_version = 'Version 2.3 for Zen Cart 1.5.6 German';
+$template_version = 'Version 2.3.1 for Zen Cart 1.5.6 German';
 $template_author = 'Picaflor-Azul.com - Anne (c) 2015 - modified 2019 by webchills for Zen Cart German';
 $template_description = 'Visit www.picaflor-azul.com for premium zen cart templates.  Learn to customize your zen cart with our Free Easy Help Tutorials.';
 $template_screenshot = '';
